6fe574c926231bb7f331edeb46f792ad102eee0b
Hikaru Takemoto <h.take1320@gmail.com>
2020-07-11 18:08:48 +0900

    :heavy_plus_sign: #3 eslintのパッケージ追加

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  rules: {
+    quotes: ["error", "single"],
+  },
+};
