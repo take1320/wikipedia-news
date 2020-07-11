cb4a9a3b9acdfde7d9ade68b2743e1c4e816c217
Hikaru Takemoto <h.take1320@gmail.com>
2020-07-11 19:04:16 +0900

    :wrench: #3 prettierの設定追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,21 @@
 module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
   ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    quotes: ["error", "single"],
+    'prettier/prettier': 'error', //ESLintでPrettierの規則もエラーとして検出する設定
+    quotes: ['error', 'single'],
   },
 };
