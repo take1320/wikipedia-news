eb6e26766ddbd997155079467fe37cb587ea784b
Hikaru Takemoto <h.take1320@gmail.com>
2020-07-18 17:06:39 +0900

    :art: #9 URLによるページの切り替え動作確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import React, { FC } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, Redirect } from 'react-router';
+import paths from 'paths';
+import Home from 'components/Home';
+import Fuga from 'components/Fuga';
 
 const App: FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+  <div>
+    <Switch>
+      <Route path={paths.home} component={Home} exact />
+      <Route path={paths.fuga} component={Fuga} exact />
+      <Redirect to={paths.home} />
+    </Switch>
   </div>
 );
 
---
+++ src/components/Fuga/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Fuga: React.FC = () => (
+  <div>
+    <p>Fuga</p>
+  </div>
+);
+
+export default Fuga;
