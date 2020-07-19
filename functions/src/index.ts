2d182d74d938936c10eae6970435bf6c904be73a
Hikaru Takemoto <h.take1320@gmail.com>
2020-07-19 16:58:45 +0900

    :sparkles: #13 APIのサンプル実装

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,24 @@
 import * as functions from 'firebase-functions';
+import admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import { collectionName } from './services/w-news/constants';
+
+admin.initializeApp();
+
+export const publishers = functions
+  .region('asia-northeast1')
+  .https.onRequest(async (req, res) => {
+    const data = { hoge: 'fuga' };
+    res.send({ data });
+  });
+
+export const articles = functions
+  .region('asia-northeast1')
+  .https.onRequest(async (req, res) => {
+    const snap = await admin
+      .firestore()
+      .collection(collectionName.articles)
+      .get();
+    const data = snap.docs.map((doc) => doc.data());
+    res.send({ data });
+  });
