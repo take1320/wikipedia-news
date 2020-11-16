732ce079f81f1759eba0b0a624dbb06693a5e9fd
hikaru takemoto <h.take1320@gmail.com>
2020-11-16 13:07:22 +0900

    :bug: #21 未使用インポートの削除

--- src/components/common/card/ArticleCard.tsx
+++ src/components/common/card/ArticleCard.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { Button, Card, Image } from 'semantic-ui-react';
+import { Button, Card } from 'semantic-ui-react';
 
 import { Article } from 'services/w-news/models/articles';
 
