d6fdabc2b4104e4c1f6fc486e8ef8219c12623c8
Hikaru Takemoto <h.take1320@gmail.com>
2020-12-17 14:57:24 +0900

    :recycle: #23 ユーザ削除コードの改善

--- functions/src/commands/truncate-auth.ts
+++ functions/src/commands/truncate-auth.ts
@@ -1,7 +1,6 @@
 import commander from 'commander';
 import admin from 'firebase-admin';
 
-import sleep from '../await-sleep';
 import serviceAccount from '../wikipedia-news-firebase-adminsdk.json';
 import { exit } from 'process';
 
@@ -13,10 +12,7 @@ const truncateAuthentication = async (): Promise<void> => {
   const users = await admin.auth().listUsers();
   console.log('users.count: ' + users.users.length);
 
-  for await (const user of users.users) {
-    await admin.auth().deleteUser(user.uid);
-    await sleep(50); // 速すぎると`QUOTA_EXCEEDED`になるので適当に間隔を開ける
-  }
+  await admin.auth().deleteUsers(users.users.map((u) => u.uid));
 
   exit();
 };
