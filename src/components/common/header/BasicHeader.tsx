e7df05e264b1cce217861a665e2109f0cc3be203
Hikaru Takemoto <h.take1320@gmail.com>
2020-12-18 13:31:19 +0900

    :art: #25 背景色やヘッダーのデザイン調整

--- src/components/common/header/BasicHeader.tsx
+++ src/components/common/header/BasicHeader.tsx
@@ -1,22 +1,33 @@
 import React, { FC, useContext } from 'react';
 import { Menu, Container } from 'semantic-ui-react';
+import styled from '@emotion/styled';
 
 import { UserContext } from '../../../contexts';
 
+const MenuHeader = styled(Menu)`
+  &&& {
+    background: #eaecf0;
+    border-radius: 0;
+  }
+`;
+
+const MenuTitle = styled(Menu.Item)`
+  &&& {
+    color: #4f5051;
+    font-family: 'Linux Libertine', Georgia, Times, serif;
+    font-size: 1.3rem;
+  }
+`;
+
 const BasicHeader: FC = () => {
-  const hoge = useContext(UserContext);
-  console.log(hoge);
+  const user = useContext(UserContext);
+  console.log(user);
   return (
-    <Menu borderless>
+    <MenuHeader borderless>
       <Container text>
-        <Menu.Item header>Wikipedia in news</Menu.Item>
-        <button
-          onClick={() => {
-            alert('hoge' + hoge.user?.name);
-          }}
-        />
+        <MenuTitle header>ウィキペディア in News</MenuTitle>
       </Container>
-    </Menu>
+    </MenuHeader>
   );
 };
 export default BasicHeader;
